d920c0bbeaba1943d08b8e8a5e1fde098d5c7632
Andrej <andrejvelickovski.35@gmail.com>
2024-09-27 01:11:44 +0200

    Testing notes for solution

--- main.py
+++ main.py
@@ -75,3 +75,8 @@ with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
         writer.writerow(product)
 
 print("Scraping successful")
+
+# Za testiranje na tocnost i preciznost na aplikacijata napravete gi slednite cekori:
+# 1. namalete go brojot za len(products) da se dvizi pomegju 5 - 20
+# 2. row 74 - products[:5-20?]
+# 3. driver = webdriver.Chrome() -> dokolku koristite Windows mozebi bi imale potreba od stavanje na pateka kade se naogja Chrome instalacija, na Linux nema problem
